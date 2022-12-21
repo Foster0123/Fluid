accd8b5e22501ad104b78ea5ada430748a57c6dd
Foster <72973015+Foster0123@users.noreply.github.com>
2022-12-21 21:43:40 +0530

    Fluid Chat Commit 4th Chat Page Updated Styles

--- src/client/app/components/chat/chat.component.ts
+++ src/client/app/components/chat/chat.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 interface ChatInterface {
   id : number
   uname : string,
-  status : string
+  status : string,
+  profile : string
 }
 @Component({
   selector: 'app-chat',
@@ -12,7 +13,17 @@ interface ChatInterface {
 
 export class ChatComponent {
   chats : ChatInterface [] = [
-    { id: 1, uname:"Something0123", status:"Active Now" },
-    { id: 2, uname:"SomethingElse0123", status:"Active 1hr Ago" }
+    { id: 1, uname:"Something0123", status:"Active Now", profile: "./assets/images/blaze.png" },
+    { id: 2, uname:"SomethingElse0123", status:"Active 1hr Ago", profile: "./assets/images/fluid.png" },
+    { id: 1, uname:"Something0123", status:"Active Now", profile: "./assets/images/blaze.png" },
+    { id: 1, uname:"Something0123", status:"Active Now", profile: "./assets/images/blaze.png" },
+    { id: 1, uname:"Something0123", status:"Active Now", profile: "./assets/images/blaze.png" },
+    { id: 1, uname:"Something0123", status:"Active Now", profile: "./assets/images/blaze.png" },
+    { id: 1, uname:"Something0123", status:"Active Now", profile: "./assets/images/blaze.png" },
+    { id: 1, uname:"Something0123", status:"Active Now", profile: "./assets/images/blaze.png" },
+    { id: 1, uname:"Something0123", status:"Active Now", profile: "./assets/images/blaze.png" },
+    { id: 1, uname:"Something0123", status:"Active Now", profile: "./assets/images/blaze.png" },
+    { id: 1, uname:"Something0123", status:"Active Now", profile: "./assets/images/blaze.png" },
+    { id: 1, uname:"Something0123", status:"Active Now", profile: "./assets/images/blaze.png" }
   ]
 }

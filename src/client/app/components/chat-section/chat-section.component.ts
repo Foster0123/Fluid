e9448a3447108a8ad739e45db795943d9617bf50
Foster <72973015+Foster0123@users.noreply.github.com>
2023-01-13 01:25:21 +0530

    Fluid Modules Added & UI Development(Chat Section Component) In Progress 4

--- src/client/app/components/chat-section/chat-section.component.ts
+++ src/client/app/components/chat-section/chat-section.component.ts
@@ -4,7 +4,18 @@ import { Component } from '@angular/core';
   selector: 'app-chat-section',
   templateUrl: './chat-section.component.html',
   styleUrls: ['./chat-section.component.scss']
-})
+}) 
 export class ChatSectionComponent {
-
+  msgs = [
+    {text: "Something-0", sentByClient:false},
+    {text: "Something-1", sentByClient:false},
+    {text: "Something-2", sentByClient:true},
+    {text: "Something-3", sentByClient:false},
+    {text: "Something-4", sentByClient:false},
+    {text: "Something-5", sentByClient:false},
+    {text: "Something-6", sentByClient:true},
+    {text: "Something-7", sentByClient:true},
+    {text: "Something-8", sentByClient:false},
+    {text: "Something-9", sentByClient:true},
+  ]
 }
